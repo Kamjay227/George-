d7f70969219e3628c25bf251b539b027f6487628
Kamjay227 <kamajay39@gmail.com>
2026-06-14 13:09:49 +0300

    Refactor box movement to include vertical direction

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let x = 0;
 let x2 = 200;
-let direction = 1;
-let direction2 = -1;
+let y = 0;
+let y2 = 150;
+let dirX = 1;
+let dirX2 = -1;
+let dirY = 1;
+let dirY2 = -1;
 let hue = 0;
 let hue2 = 180;
 
 function moveBox() {
-  x += 2 * direction;
-  x2 += 2 * direction2;
+  x += 2 * dirX;
+  x2 += 2 * dirX2;
+  y += 2 * dirY;
+  y2 += 2 * dirY2;
 
-  if (x > 400 || x < 0) direction *= -1;
-  if (x2 > 400 || x2 < 0) direction2 *= -1;
+  if (x > 400 || x < 0) dirX *= -1;
+  if (x2 > 400 || x2 < 0) dirX2 *= -1;
+  if (y > 300 || y < 0) dirY *= -1;
+  if (y2 > 300 || y2 < 0) dirY2 *= -1;
 
   hue += 2;
   hue2 += 2;
@@ -21,9 +29,11 @@ function moveBox() {
   const box2 = document.getElementById("box2");
 
   box.style.left = x + "px";
+  box.style.top = y + "px";
   box.style.backgroundColor = "hsl(" + hue + ", 100%, 50%)";
 
   box2.style.left = x2 + "px";
+  box2.style.top = y2 + "px";
   box2.style.backgroundColor = "hsl(" + hue2 + ", 100%, 50%)";
 
   requestAnimationFrame(moveBox);
