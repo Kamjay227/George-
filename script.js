4a3298a603a2be59f9ddf8d6f48e93cb9fa17a40
Kamjay227 <kamajay39@gmail.com>
2026-06-14 12:47:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let x = 0;
 let direction = 1;
+let hue = 0;
 
 function moveBox() {
-  x += 3 * direction;
+  x += 2 * direction;
   if (x > 500 || x < 0) direction *= -1;
-  document.getElementById("box").style.left = x + "px";
+
+  hue += 2;
+  if (hue > 360) hue = 0;
+
+  const box = document.getElementById("box");
+  box.style.left = x + "px";
+  box.style.backgroundColor = "hsl(" + hue + ", 100%, 50%)";
+
   requestAnimationFrame(moveBox);
 }
 
