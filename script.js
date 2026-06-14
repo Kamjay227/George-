3666f8a41246d0475bef6a8b5ac61c84c3f0b43b
Kamjay227 <kamajay39@gmail.com>
2026-06-14 12:18:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-window.onload = function () {
-    let box = document.querySelector(".blue-square");
+let box = document.querySelector(".blue-square");
 
-    let position = 0;
-    let speed = 2;
+let position = 0;
+let speed = 2;
 
-    setInterval(() => {
-        position = position + speed;
+setInterval(() => {
+    position += speed;
 
-        if (position >= 300 || position <= 0) {
-            speed = speed * -1;
-        }
+    if (position > 300 || position < 0) {
+        speed = -speed;
+    }
 
-        box.style.left = position + "px";
-    }, 20);
-};
+    box.style.left = position + "px";
+}, 20);
