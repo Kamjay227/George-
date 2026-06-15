135c827afde2a7530fc651f343438f7424683684
Kamjay227 <kamajay39@gmail.com>
2026-06-15 22:21:24 +0300

    Add circle and line drawing to canvas
    Added code to draw a circle and a line on the canvas.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-// Draw a red rectangle
+// Rectangle
 ctx.fillStyle = "red";
 ctx.fillRect(50, 50, 150, 100);
+
+// Circle
+ctx.beginPath();
+ctx.arc(350, 100, 60, 0, Math.PI * 2);
+ctx.fillStyle = "blue";
+ctx.fill();
+
+// Line
+ctx.beginPath();
+ctx.moveTo(50, 250);
+ctx.lineTo(450, 250);
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 4;
+ctx.stroke();
